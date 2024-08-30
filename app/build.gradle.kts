91e324687e65bfb2712a0c43a25e5cf73467a1b6
Phung Nguyen <nhphung216@gmail.com>
2024-08-30 15:04:51 +0700

    setup project structure, add unit test with repository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,18 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation("androidx.test:core:1.6.1")
+    testImplementation("org.mockito:mockito-core:4.6.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    testImplementation("io.mockk:mockk:1.13.5")
 }
--- app/src/test/java/com/phung/catastrophicapp/CatRepositoryTest.kt
+++ app/src/test/java/com/phung/catastrophicapp/CatRepositoryTest.kt
@@ -1,4 +1,35 @@
 package com.phung.catastrophicapp
 
+import com.phung.catastrophicapp.data.local.CatImageDao
+import com.phung.catastrophicapp.data.remote.ApiService
+import com.phung.catastrophicapp.data.repository.CatRepositoryImpl
+import com.phung.catastrophicapp.domain.model.CatImage
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
 class CatRepositoryTest {
+
+    private lateinit var apiService: ApiService
+
+    private lateinit var catImageDao: CatImageDao
+
+    private lateinit var catRepository: CatRepositoryImpl
+
+    @Before
+    fun setup() {
+        apiService = Mockito.mock(ApiService::class.java)
+        catImageDao = Mockito.mock(CatImageDao::class.java)
+        catRepository = CatRepositoryImpl(apiService, catImageDao)
+    }
+
+    @Test
+    fun testFetchCatImagesFromApi() = runBlocking {
+        // mock data
+        val catImages = listOf<CatImage>()
+
+        // call api
+        catRepository.fetchCatImages(1)
+    }
 }
